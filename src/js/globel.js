77f873309e928fb3e4f62d5e2d24e3e25e6fb302
slightlovery <huainianshanghen@163.com>
2017-07-08 17:31:33 +0800

    4th commit

--- src/js/globel.js
+++ src/js/globel.js
@@ -10,7 +10,10 @@ $(function(){
         var _this = $(this);
         var index = _this.index();
         var tabs = _this.parent('.u_tabs');
+        var panels = tabs.siblings('.u_panels');
         tabs.find('.u_tab.u_active').removeClass('u_active');
         _this.addClass('u_active');
+        panels.find('>.u_panel.u_active').removeClass('u_active');  //'>',Child Selectors 子对象选择符,所有子对象
+        panels.find('>.u_panel').eq(index).addClass('u_active');
     })
 });
